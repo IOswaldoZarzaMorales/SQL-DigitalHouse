dbd4e9f0495ab192058630c8a3b9c4990a7690fc
Oswaldo Ivan Zarza Morales <OSWALDOMORALES26@HOTMAIL.COM>
2024-04-02 01:57:14 -0600

    first commit

---
+++ clase.js
@@ -0,0 +1,72 @@
+//Este es un comentario 
+
+/* if (esPar){
+    console.log("Es par");
+}  */
+
+/* let apellido = "Lopez"
+let nombre = "Juan" //String o cadena de caracteres "" o ''
+let edad = 35 //Tipo number
+
+const DNI = 1234 //si declaro como const no puedo reasignar
+// -----> Es una buena practica declarar constantes como mayusculas 
+
+//Funciones nativas 
+
+//Alert
+//Prompt 
+
+console.log(nombre);
+console.log(18);
+console.log(edad + 2);
+console.log(edad + "2");
+let dniIngresado = prompt("Ingrese su DNI ")
+
+
+alert("Bienvenido "  + nombre) */
+
+/* let edad = Number(prompt("Ingrese su edad"))
+
+ console.log(edad);
+edad = Number(edad)
+console.log(edad); 
+let anioNacimiento = 2024 - edad
+alert("Usted nacio en el: " + anioNacimiento)
+alert("El año que vienen va a cumplir  " + (+ edad + 1))
+
+//Number, parseInt parseFloat -----> Metodos para strings a numeros
+//typeof indicac el tipo de dato 
+
+let telefono = 12345 
+console.log(typeof telefono);
+
+
+//Tipos de datos 
+//Number  => 3   6.2
+//String "Hola"
+//Booleanos true/ false 
+
+let tieneMascotas = true;
+console.log(tieneMascotas);
+console.log(typeof tieneMascotas); */
+
+let nombre = prompt('Ingrese su nombre'); 
+nombre = nombre.toUpperCase() //------> Metodo para convertir string en mayusculas  
+console.log(nombre);
+
+let nombre2 = prompt('Ingrese su nombre2'); 
+nombre2 = nombre2.toLocaleLowerCase() //------> Metodo para convertir string en minusculas 
+console.log(nombre2);
+
+console.log(nombre[0]);
+console.log(nombre[1]);
+console.log(nombre[2]);
+console.log(nombre[3]);
+
+
+
+
+
+
+
+
